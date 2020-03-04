bef1f6c794a8fd7fef604d8a333cd77d117433a0
ahmad kmal <eng.ahmadkmal@gmail.com>
2020-03-04 16:31:48 +0200

    add catalog js

--- js/app.js
+++ js/app.js
@@ -8,8 +8,9 @@ var Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
-  new CartItem(product, quantity);
-  this.items = product;
+  var thisItem = new CartItem(product, quantity);
+  this.items.push(thisItem);
+   
 };
 
 Cart.prototype.saveToLocalStorage = function() {
@@ -20,7 +21,7 @@ Cart.prototype.saveToLocalStorage = function() {
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
-  
+
 };
 
 var CartItem = function(product, quantity) {
--- js/catalog.js
+++ js/catalog.js
@@ -27,7 +27,7 @@ function populateForm() {
 function handleSubmit(event) {
 
   // TODO: Prevent the page from reloading
-
+  event.preventDefault();
   // Do all the things ...
   addSelectedItemToCart();
   cart.saveToLocalStorage();
@@ -39,17 +39,37 @@ function handleSubmit(event) {
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
+  // for (var i in Product.allProducts) {
+  //   if(event.target.items.value === Product.allProducts[i].name)
+  //   {
+  //  var pickedItem = event.target.items.value;
+  // var quantity =  Number();
+  // var cart1 = new Cart(event.target.items.value);
+  cart.addItem(event.target.items.value,event.target.quantity.value);
+  // console.log(` picked item ${pickedItem} , quantity ${quantity}`);
+  // return
+  // }
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
+  
+// }
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
+function updateCounter() {
+  document.getElementById('itemCount').textContent = cart.items.length ;
+  console.log(cart.items.length);
+}
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
   // TODO: Get the item and quantity from the form
   // TODO: Add a new element to the cartContents div with that information
+  var cartContent = document.getElementById('cartContents'); 
+  var pEl = document.createElement('p');
+  pEl.textContent = ` you have added a ${event.target.quantity.value}  of ${event.target.items.value}`;
+  cartContent.appendChild(pEl);
+
 }
 
 // Set up the "submit" event listener on the form.
@@ -61,3 +81,4 @@ catalogForm.addEventListener('submit', handleSubmit);
 // Before anything else of value can happen, we need to fill in the select
 // drop down list in the form.
 populateForm();
+
